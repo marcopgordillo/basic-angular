56c236cc935c16bf4671c0c62bea36c7d93a8cd7
Marco Gordillo <marcopgordillo@gmail.com>
2018-10-31 12:36:26 -0500

    Added String Interpolation. closes #11

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,9 +7,16 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getServerStatus(): string {
+    return this.serverStatus;
   }
 
 }
