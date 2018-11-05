19a748ccdf27447ec9e5a956ee106d11cb30ae47
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-05 09:54:00 -0500

    Added Custom property binding. #40

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ServerModel} from "../server.model";
 
 @Component({
   selector: 'app-server-element',
@@ -6,6 +7,9 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
+
+  @Input()
+  private element: ServerModel;
 
   constructor() { }
 
