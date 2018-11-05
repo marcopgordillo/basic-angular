ccbfacdc66b0a854f7aac80881f8ea0d601137d1
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-05 09:31:53 -0500

    App before split. #39

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,19 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css', '/somepath/anotherstyle.css']
-  styles: [`
-    h3 {
-      color: dodgerblue;
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'basic-angular';
+  private newServerName:string = '';
+  private newServerContent:string = '';
+
+  private serverElements: {name: string, type: string, content: string}[] = [];
+
+  public onAddServer(): void {
+    this.serverElements.push({name: this.newServerName, type: 'server', content: this.newServerContent});
+  }
+
+  public onAddBlueprint(): void {
+    this.serverElements.push({name: this.newServerName, type: 'blueprint', content: this.newServerContent});
+  }
 }
