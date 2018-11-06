98c5f19f75ce24bd62c7c0efcf2c6c7c0ad8ca57
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-06 16:52:33 -0500

    Building and Using a Dropdown Directive. closes #64

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,22 @@
+import {Directive, HostListener, QueryList, ViewChildren} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+  exportAs: 'appDropdown'
+})
+export class DropdownDirective {
+
+  //@HostBinding('class.show')
+  private isOpen: boolean = false;
+
+  @ViewChildren('div')
+  private viewChildren: QueryList<any>;
+
+  constructor() { }
+
+  @HostListener('click')
+  public toggleOpen() {
+    this.isOpen = !this.isOpen;
+    this.viewChildren.forEach(item => console.log(item));
+  }
+}
