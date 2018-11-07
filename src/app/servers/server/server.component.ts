06a13c0f7ad983ffa8cb0abacde63a1d1f16b59f
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-07 16:23:43 -0500

    Practicing and some Common Gotchas. closes #92

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,19 +1,35 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 import {ServersService} from '../servers.service';
+import {ActivatedRoute, Params} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
-export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+export class ServerComponent implements OnInit, OnDestroy {
 
-  constructor(private serversService: ServersService) { }
+  private server: {id: number, name: string, status: string};
+  paramsSubscription: Subscription;
+
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+
+    this.paramsSubscription = this.route.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(+params['id']);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
