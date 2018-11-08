b68972eb06726c0df27ee5fa9a7e08e0f3ee4719
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-08 17:57:07 -0500

    To continue in house. #146

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,6 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private subscriptions: string[] = ['Basic', 'Advanced', 'Pro'];
+  private selectedSubscription: string = 'Advanced';
 }
