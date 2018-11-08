fa7c2c1029ac0b507c905805967628978e3cc2da
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-08 02:24:32 -0500

    Programmatic Navigation to the Edit Page. closes #118

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {ActivatedRoute, Params} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
@@ -13,16 +13,19 @@ import {Subscription} from 'rxjs';
 export class RecipeDetailComponent implements OnInit, OnDestroy {
 
   private recipe: Recipe;
+  private id: number;
   private paramsSubscription: Subscription;
 
   constructor(private recipeService: RecipeService,
+              private router: Router,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
 
     this.paramsSubscription = this.route.params.subscribe(
       (params: Params) => {
-        this.recipe = this.recipeService.getRecipe(+params['id']);
+        this.id = +params['id'];
+        this.recipe = this.recipeService.getRecipe(this.id);
       }
     );
   }
@@ -33,5 +36,10 @@ export class RecipeDetailComponent implements OnInit, OnDestroy {
 
   public onAddToShoppingList(): void {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+    // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
   }
 }
