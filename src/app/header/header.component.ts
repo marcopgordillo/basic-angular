e82ff50c8b4330d26b9ac743dc1ce16160e63b02
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-08 00:49:41 -0500

    Adding Navigation to the App. closes #108

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,24 +1,9 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  private current: string = 'recipe';
-
-  @Output()
-  private featureSelected: EventEmitter<string> = new EventEmitter<string>();
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  public onSelect(feature: string): void {
-    this.featureSelected.emit(feature);
-    this.current = feature;
-  }
-
+export class HeaderComponent {
 }
