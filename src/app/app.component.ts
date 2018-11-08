65e1e38432fe28f46ec19f20f8a8cc7ad58e9774
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-08 10:48:52 -0500

    Using Subjects to Pass AND Listen to Data. closes #126

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UsersService} from "./users.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  user1Activated = false;
+  user2Activated = false;
+  userActivatedSubscription: Subscription;
+
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit() {
+    this.userActivatedSubscription = this.usersService.userActivated.subscribe(
+      (id: number) => {
+        if (id === 1) {
+          this.user1Activated = true;
+        } else if (id === 2) {
+          this.user2Activated = true;
+        }
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.userActivatedSubscription.unsubscribe();
+  }
+
 }
