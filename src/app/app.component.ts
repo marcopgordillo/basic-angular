d5a3fbdf08f57f51a14dc701e5f6cd5785ae3b3c
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-09 12:13:35 -0500

    Assignment 7: Practicing Reactive Forms. closes #160

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {CustomValidators} from "./custom-validators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private statuses: string[] = ['stable', 'critical', 'finished'];
+  private projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null,
+        [Validators.required, CustomValidators.invalidProjedtName],
+        CustomValidators.asyncInvalidProjectName),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl('stable')
+    });
+  }
+
+  onSaveProject() {
+    console.log(this.projectForm.value);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from "@angular/forms";
 
 import {AppComponent} from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -9,6 +11,7 @@ import {AppComponent} from './app.component';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
