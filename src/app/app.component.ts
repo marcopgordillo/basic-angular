7b1be7fe3bb0042b4c47e7233012925f11cce201
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-09 11:22:15 -0500

    Reactive: Setting and Patching Values. closes #159

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,17 +22,25 @@ export class AppComponent implements OnInit{
       'hobbies': new FormArray([])
     });
 
-    /*this.signupForm.valueChanges.subscribe(
-      (value) => console.log(value)
-    );*/
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Max',
+        'email': 'max@test.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
 
-    /*this.signupForm.statusChanges.subscribe(
-      (status) => console.log(status)
-    );*/
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Anna'
+      }
+    });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby() {
