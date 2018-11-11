3ba466ad65ec2d09c9933386e96112f3ac6058a9
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-11 08:46:37 -0500

    Using Pipes. closes #181

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {ServerModel} from './server.model';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,18 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private servers: ServerModel[] = [
+    new ServerModel('medium', 'Production Server', 'stable', new Date(2017, 1, 15)),
+    new ServerModel('large', 'User Database', 'stable', new Date(2017, 1, 15)),
+    new ServerModel('small', 'Development Server', 'offline', new Date(2017, 1, 15)),
+    new ServerModel('small', 'Testing Server', 'critical', new Date(2017, 1, 15))
+  ];
+
+  getStatusClasses(server: ServerModel) {
+    return {
+      'list-group-item-success' : server.status === 'stable',
+      'list-group-item-warning' : server.status === 'offline',
+      'list-group-item-danger' : server.status === 'critical',
+    };
+  }
 }
---
+++ src/app/server.model.ts
@@ -0,0 +1,13 @@
+export class ServerModel {
+  instanceType: string;
+  name: string;
+  status: string;
+  started: Date;
+
+  constructor(instanceType: string, name: string, status: string, started: Date) {
+    this.instanceType = instanceType;
+    this.name = name;
+    this.status = status;
+    this.started = started;
+  }
+}
