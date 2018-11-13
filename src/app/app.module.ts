dfbb2ef4a26b8fc7f85df699a8cd2637eac1efa3
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-12 20:31:41 -0500

    Creating a Shopping List Feature Module. closes #222

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
 import {BsDropdownModule, CollapseModule} from 'ngx-bootstrap';
 import {HttpClientModule} from '@angular/common/http';
 import {DataStorageService} from './shared/data-storage.service';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 import {RecipeService} from './recipes/recipe.service';
@@ -20,25 +17,24 @@ import {AuthGuardService} from './auth/auth-guard.service';
 import {CanDeactivateGuardService} from './auth/can-deactivate-guard.service';
 import {RecipesModule} from './recipes/recipes.module';
 import {SharedModule} from './shared/shared.module';
+import {ShoppingListModule} from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     SignupComponent,
     SigninComponent,
     NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     BsDropdownModule.forRoot(),
     CollapseModule,
     HttpClientModule,
     RecipesModule,
     SharedModule,
+    ShoppingListModule,
     AppRoutingModule
   ],
   providers: [
