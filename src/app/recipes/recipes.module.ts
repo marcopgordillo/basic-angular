376918679a40ff63ad582b7847bf3795ea643097
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-12 20:16:44 -0500

    Creating a Shared Module. closes #221

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,8 +1,9 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
-import {RecipesRoutingModule} from './recipes-routing.module';
+import {BsDropdownModule, CollapseModule} from 'ngx-bootstrap';
 
+import {RecipesRoutingModule} from './recipes-routing.module';
 import {RecipesComponent} from './recipes.component';
 import {RecipeStartComponent} from './recipe-start/recipe-start.component';
 import {RecipeListComponent} from './recipe-list/recipe-list.component';
@@ -10,7 +11,6 @@ import {RecipeEditComponent} from './recipe-edit/recipe-edit.component';
 import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
 import {RecipeItemComponent} from './recipe-list/recipe-item/recipe-item.component';
 import {SharedModule} from '../shared/shared.module';
-import {BsDropdownModule, CollapseModule} from 'ngx-bootstrap';
 
 
 @NgModule({
