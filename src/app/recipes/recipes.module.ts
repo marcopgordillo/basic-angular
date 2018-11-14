39f0469edf6a212cb26adbc2ac19e953770bfd02
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-14 15:53:28 -0500

    Lazy Load and Dynamic Injection. closes #284

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,15 +1,17 @@
-import {NgModule} from '@angular/core';
-import {ReactiveFormsModule} from '@angular/forms';
-import {BsDropdownModule, CollapseModule} from 'ngx-bootstrap';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BsDropdownModule, CollapseModule } from 'ngx-bootstrap';
+import { StoreModule } from '@ngrx/store';
 
-import {RecipesRoutingModule} from './recipes-routing.module';
-import {RecipesComponent} from './recipes.component';
-import {RecipeStartComponent} from './recipe-start/recipe-start.component';
-import {RecipeListComponent} from './recipe-list/recipe-list.component';
-import {RecipeEditComponent} from './recipe-edit/recipe-edit.component';
-import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
-import {RecipeItemComponent} from './recipe-list/recipe-item/recipe-item.component';
-import {SharedModule} from '../shared/shared.module';
+import { RecipesRoutingModule } from './recipes-routing.module';
+import { RecipesComponent } from './recipes.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { SharedModule } from '../shared/shared.module';
+import { recipeReducer } from './store/recipe.reducers';
 
 
 @NgModule({
@@ -26,6 +28,7 @@ import {SharedModule} from '../shared/shared.module';
     BsDropdownModule.forRoot(),
     CollapseModule,
     SharedModule,
+    StoreModule.forFeature('recipes', recipeReducer),
     RecipesRoutingModule
   ]
 })
