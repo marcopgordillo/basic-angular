6b3773eb904af020ad9c50d87643493c9766554f
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-15 16:43:44 -0500

    Setting up the Starting Project. closes #302

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -9,6 +12,9 @@ import {AppComponent} from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
