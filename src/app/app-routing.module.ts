29b99632fb5cfa932f03c3273b8394dd01045ca2
Marco Gordillo <marcopgordillo@gmail.com>
2018-11-17 00:27:29 -0500

    Custom project.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'users', loadChildren: './users/user.module#UserModule' }
+  { path: 'users', loadChildren: './users/users.module#UsersModule' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
     HomeComponent
   ],
   imports: [
